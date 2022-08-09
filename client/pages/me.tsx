6979d558cb624cddad31773916d30879aff0f761
portador <deus7@protonmail.com>
2022-08-09 21:44:07 +0300

    client init next project with tailwind

---
+++ client/pages/me.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Me = () => {
+    return (
+        <main className="flex items-center justify-center h-full">
+            <div className="space-y-4 text-center">
+                <h1 className="px-4 py-2 text-lg font-medium bg-gray-200 rounded">
+                    Client side authentication
+                </h1>
+            </div>
+        </main>
+    );
+};
+
+export default Me;
